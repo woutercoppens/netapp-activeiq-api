961142f67cb17aaccc4853555129f7eb1c2cfab5
Wouter Coppens <wouter.coppens@gmail.com>
2022-08-31 09:54:16 +0200

    update to pandas 1.4

--- samples/get-storeff.py
+++ samples/get-storeff.py
@@ -14,7 +14,7 @@ import pandas as pd
 from pprint import pprint
 
 from flatten_dict import flatten
-from flatten_dict.reducer import make_reducer
+from flatten_dict.reducers import make_reducer
 
 # logging.basicConfig(level=os.environ.get("LOGLEVEL", "DEBUG"))
 
@@ -29,6 +29,12 @@ def write2csv(filename, data):
     df = pd.DataFrame(data)
     df.to_csv(filename)
 
+def add_extra_data(sl, data):
+    rsl = []
+    for d in sl:
+        d.update(data)
+        rsl.append(d)
+    return rsl
 
 def clean_data(data, fields=None):
     """
@@ -41,7 +47,7 @@ def clean_data(data, fields=None):
         df = pd.json_normalize(data[i + 1])
         if fields:
             df = df[fields]
-        table = table.append(df)
+        table = pd.concat([table, df])
     return table
 
 
@@ -73,9 +79,12 @@ if __name__ == "__main__":
             cust_id = row["customer_id"]
             sl = aiq.SystemApi.get_system_details_by_level(level="customer", id=cust_id)
             systems = sl["results"]
+            print(type(systems))
+            print(systems)
             extra_data = {"customer_id": cust_id}
-            systems.update(extra_data)  # add the customer id to the system
-            tablesl = tablesl.append(clean_data(systems))
+            # systems.update(extra_data)  # add the customer id to the system
+            systems = add_extra_data(systems, extra_data)  # add the customer id to the system
+            tablesl = pd.concat([tablesl,clean_data(systems)])
         # pprint(tablesl)
         write2csv("/home/wouter/netapp/systems.csv", tablesl)
 
@@ -91,12 +100,12 @@ if __name__ == "__main__":
             sl = aiq.StorageEfficiencyApi.get_efficiency_details_by_level(
                 level="serial_numbers", id=sys_id
             )
-
-            pprint(sl)
             results = sl["results"]
             extra_data = {"serial_number": sys_id}
-            results.update(extra_data)  # add the serial number to the efficiency data
-            tablesl = tablesl.append(results, ignore_index=True)
+            # print(results)
+            # print(type(results))
+            results = add_extra_data(results, extra_data)  # add the serial number to the efficiency data
+            tablesl = pd.concat([tablesl,results], ignore_index=True)
         pprint(tablesl)
         write2csv("/home/wouter/netapp/storage_efficiencies_systems.csv", tablesl)
 
@@ -120,7 +129,7 @@ if __name__ == "__main__":
             systems = sl["clusters"]
             # extra_data= {'customer_id': cust_id}
             # systems.update(extra_data) #add the customer id to the system
-            tablesl = tablesl.append(clean_data(systems))
+            tablesl = pd.concat([tablesl, clean_data(systems)])
         # pprint(tablesl)
         write2csv("/home/wouter/netapp/clusters.csv", tablesl)
 
@@ -140,7 +149,7 @@ if __name__ == "__main__":
             pprint(sl)
             results = sl["results"]
             extra_data = {"cluster_id": sys_id}
-            results.update(extra_data)  # add the serial number to the efficiency data
-            tablesl = tablesl.append(results, ignore_index=True)
+            results = add_extra_data(results, extra_data)  # add the serial number to the efficiency data
+            tablesl = pd.concat([tablesl, results], ignore_index=True)
         pprint(tablesl)
         write2csv("/home/wouter/netapp/storage_efficiencies_clusters.csv", tablesl)
